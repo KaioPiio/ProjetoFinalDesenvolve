1b2569e6e82584a2f0b474180c77008471725c93
KaioPiio <kaiopio13@gmail.com>
2026-03-24 22:35:45 -0300

    Categorias de Produto

---
+++ frontend-catalog/src/components/CategoriesBar.tsx
@@ -0,0 +1,19 @@
+import "./CategoriesBars.css";
+
+interface Props {
+    onSelectCategory: (category: string) => void;
+}
+
+function CategorieBar({ onSelectCategory }: Props) {
+    return (
+        <div className="categories-bar">
+            <button onClick={() => onSelectCategory("electronics")}>Eletrônicos</button>
+            <button onClick={() => onSelectCategory("jewelery")}>Joias</button>
+            <button onClick={() => onSelectCategory("men's clothing")}>Roupas Masculinas</button>
+            <button onClick={() => onSelectCategory("women's clothing")}>Roupas Femininas</button>
+            <button onClick={() => onSelectCategory("")}>Todos</button> 
+        </div>
+    );
+}
+
+export default CategorieBar;
--- frontend-catalog/src/pages/ProductList.tsx
+++ frontend-catalog/src/pages/ProductList.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
+import CategorieBar from "../components/CategoriesBar";
+
 
 interface Product {
   id: number;
@@ -10,20 +12,35 @@ interface Product {
 
 function ProductList() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [category, setCategory] = useState<string>(""); // Estado para categoria selecionada
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
+
+    const url = category 
+    ? `https://fakestoreapi.com/products/category/${encodeURIComponent(category)}` 
+    : "https://fakestoreapi.com/products";
+    
+    fetch(url)
       .then(res => res.json())
-      .then(data => setProducts(data));
-  }, []);
+      .then(data => setProducts(data))
+      .catch(err => console.error("Erro ao buscar produtos:", err));
+  }, [category]); // Reexecuta quando a categoria mudar
+
+  if (products.length === 0) return <p>Carregamento...</p>;
 
   return (
-    <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gap: "20px" }}>
-      {products.map(product => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div>
+      {/* Barra de categorias */}
+      <CategorieBar onSelectCategory={setCategory} />
+
+      {/* Lista de produtos */}
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gap: "20px" }}>
+        {products.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default ProductList;
+  export default ProductList;     
